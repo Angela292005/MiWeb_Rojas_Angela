9358eacc9a0645bbfd58becfe6350e3971789809
Angela Rojas <71849594@continental.edu.pe>
2026-03-23 21:38:56 -0500

    Modificado

--- suma.php
+++ suma.php
@@ -1,39 +1,7 @@
 <?php
-$a = 5;
-$b = 3;
+$a = 14;
+$b = 5;
 $suma = $a + $b;
+
+echo "La suma es: " . $suma;
 ?>
-
-<!DOCTYPE html>
-<html>
-<head>
-    <title>Suma</title>
-    <style>
-        body {
-            background-color: #f0f8ff;
-            font-family: Arial;
-            text-align: center;
-        }
-        .card {
-            background: white;
-            padding: 20px;
-            margin: 100px auto;
-            width: 300px;
-            border-radius: 10px;
-            box-shadow: 0px 0px 10px gray;
-        }
-        h1 {
-            color: #0077b6;
-        }
-    </style>
-</head>
-<body>
-
-<div class="card">
-    <h1>Resultado</h1>
-    <p>La suma de <?php echo $a; ?> + <?php echo $b; ?> es:</p>
-    <h2><?php echo $suma; ?></h2>
-</div>
-
-</body>
-</html>
