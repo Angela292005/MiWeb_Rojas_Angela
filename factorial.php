bb679df92c70a5fe786c2ae1df038aa6994fea87
Angela Rojas <71849594@continental.edu.pe>
2026-03-23 21:34:56 -0500

    Modificado

--- factorial.php
+++ factorial.php
@@ -6,31 +6,5 @@ for ($i = 1; $i <= $num; $i++) {
     $factorial *= $i;
 }
 
-echo "
-<!DOCTYPE html>
-<html>
-<head>
-<style>
-body { font-family: Arial; text-align:center; background:#ffe; }
-.card {
-    margin:100px auto;
-    padding:20px;
-    width:300px;
-    background:white;
-    border-radius:10px;
-    box-shadow:0 0 10px gray;
-}
-</style>
-</head>
-<body>
-
-<div class='card'>
-<h2>Factorial</h2>
-<p>El factorial de $num es:</p>
-<h3>$factorial</h3>
-</div>
-
-</body>
-</html>
-";
+echo "El factorial es: " . $factorial;
 ?>
